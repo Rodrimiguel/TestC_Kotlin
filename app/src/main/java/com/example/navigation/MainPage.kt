224f429fb02dc7b157bb3e575df049fa54745321
RoderickMFoggia <rodrigomfoggia84@gmail.com>
2024-05-07 14:40:40 -0300

    Update Navhost

--- app/src/main/java/com/example/navigation/MainPage.kt
+++ app/src/main/java/com/example/navigation/MainPage.kt
@@ -1,14 +1,52 @@
 package com.example.navigation
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.navigation.ui.theme.NavigationTheme
+
+@OptIn(ExperimentalMaterial3Api::class)
 
 @Composable
 fun MainPage(modifier: Modifier = Modifier) {
 
+    val navController = rememberNavController()
+
+
+
+    Scaffold(
+        modifier = modifier,
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = "Cualquier cosa")
+                },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    titleContentColor = MaterialTheme.colorScheme.primary,
+                    containerColor = MaterialTheme.colorScheme.primaryContainer
+                )
+            )
+        }
+    ) {
+        NavHost(
+            navController = navController,
+            modifier = Modifier.padding(it),
+            startDestination = "page1"
+        ) {
+            composable("page1") { Page1() }
+            composable("page2") { Page2() }
+        }
+    }
 }
 
 @Preview(showBackground = true)
